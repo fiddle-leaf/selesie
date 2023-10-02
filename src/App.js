de042dbb6ea5971556f455e9d97a7ae758d32073
fiddle-leaf <shanonps@icloud.com>
2023-10-02 00:29:08 -0400

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./components/home/Home";
+import Resume from "./components/resume/Resume";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="resume" />
+        <Route path="resume" element={<Resume />} />
       </Route>
     </Routes>
   );
---
+++ src/components/resume/Resume.js
@@ -0,0 +1,8 @@
+import styles from "./Resume.module.sass";
+export default function Resume() {
+  return (
+    <main className={styles.resume}>
+      <h2>Hang tight! Resume coming soon...</h2>
+    </main>
+  );
+}
