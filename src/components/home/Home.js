a9ee5d6bff8c043365b301295bf1bdae46b1f35a
fiddle-leaf <shanonps@icloud.com>
2023-10-02 00:06:26 -0400

    Finish card styling

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,23 +6,24 @@ export default function Home() {
   return (
     <main className={styles.page}>
       <article className={styles.intro}>
-        <h2 className={styles.title}>Hi! Welcome to my page.</h2>
+        <h2 className={styles.title}>Hi! My name is Shanon.</h2>
         <p>
-          My name is Shanon. I am a Web Developer from Rockville, Maryland in
-          love with all things color: theory, accessibility, and
-          personalization.
+          I am a Web Developer from Rockville, Maryland passionate about
+          colorful and accessible software.
         </p>
       </article>
       <article className={styles.info}>
         <h3 className={styles.subtitle}>About Me</h3>
+        <h4 className={styles.bits}>Peruana, Plant Lover, and Photographer</h4>
         <div className={styles.about}>
           <DevIcon className={styles.dev} />
-          <p>
-            My love for code originated on MySpace, where I made layouts for
-            kids at school. Since, my love for Web Design grew as
-            personalization continuously declined from modern software. My goal
-            is to bring color back into User Interface design.
-          </p>
+          <div>
+            <p>
+              My love for code originated on MySpace, where I made layouts for
+              kids at school. My goal is to bring color and personalization back
+              to UI design.
+            </p>
+          </div>
         </div>
       </article>
       <Projects />
--- src/components/home/projects/Projects.js
+++ src/components/home/projects/Projects.js
@@ -4,8 +4,37 @@ import styles from "./Projects.module.sass";
 export default function Projects() {
   return (
     <article className={styles.projects}>
-      <h3 className={styles.subtitle}>Projects on Github</h3>
+      <h3 className={styles.title}>Projects on Github</h3>
       <DevWorkIcon className={styles.work} />
+      <article className={styles.cards}>
+        <div>
+          <h4 className={styles.subtitle}>Vacation Planner</h4>
+          <a
+            href="https://github.com/fiddle-leaf/vacations"
+            alt="Vacation Planner on Github"
+          >
+            Source
+          </a>
+          <p>
+            Austin fashion axe bruh blog freegan DIY mustache tote bag tonx,
+            subway tile paleo thundercats church-key synth hot chicken.
+          </p>
+        </div>
+        <div>
+          <h4 className={styles.subtitle}>Astro Birth Day</h4>
+          <a
+            href="https://github.com/fiddle-leaf/astro-birth-day"
+            alt="Astro Birth Day on Github"
+          >
+            Source
+          </a>
+          <p>
+            Venmo crucifix williamsburg, literally af tonx mumblecore
+            single-origin coffee ethical church-key raclette. 3 wolf moon offal
+            taiyaki sustainable bruh swag.
+          </p>
+        </div>
+      </article>
     </article>
   );
 }
