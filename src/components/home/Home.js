1ef17e7804fb7aba8d83afe9e611d5871143e227
fiddle-leaf <shanonps@icloud.com>
2023-10-12 17:00:12 -0400

    Style title underline and reconfigure sass files

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -14,26 +14,28 @@ export default function Home() {
       </article>
       <div className={styles.work}>
         <article className={styles.info}>
-          <h3 className="subtitle">About Me</h3>
-          <h4 className={styles.bits}>
-            Peruana, Plant Lover, and Photographer
-          </h4>
-          <div className={styles.about}>
-            <div>
-              <img src={DevIcon} alt="UDraw Illustration - Design Thinking" />
-            </div>
-            <div>
-              <p>
-                My love for code originated on MySpace, where I made layouts for
-                kids at school. My goal is to bring color and personalization
-                back to UI design.
-              </p>
+          <div className="head">
+            <h3 className="subtitle">About Me</h3>
+          </div>
+          <div className="content">
+            <h4 className={styles.bits}>
+              Peruana, Plant Lover, and Photographer
+            </h4>
+            <div className={styles.about}>
+              <div>
+                <img src={DevIcon} alt="UDraw Illustration - Design Thinking" />
+              </div>
+              <div>
+                <p>
+                  My love for code originated on MySpace, where I made layouts
+                  for kids at school. My goal is to bring color and
+                  personalization back to UI design.
+                </p>
+              </div>
             </div>
           </div>
         </article>
-        <div className={styles.projects}>
-          <Projects />
-        </div>
+        <Projects className={styles.projects} />
       </div>
     </main>
   );
--- src/components/home/projects/Projects.js
+++ src/components/home/projects/Projects.js
@@ -4,39 +4,43 @@ import styles from "./Projects.module.sass";
 export default function Projects() {
   return (
     <article className={styles.projects}>
-      <h3 className="subtitle">Projects on Github</h3>
-      <div>
-        <img src={DevWorkIcon} alt="Work UDraw Illustration" />
+      <div className="head">
+        <h3 className="subtitle">Projects on Github</h3>
       </div>
-      <article className={styles.cards}>
+      <div className="content">
         <div>
-          <h4 className={styles.subtitle}>Vacation Planner</h4>
-          <a
-            href="https://github.com/fiddle-leaf/vacations"
-            alt="Vacation Planner on Github"
-          >
-            Source
-          </a>
-          <p>
-            Austin fashion axe bruh blog freegan DIY mustache tote bag tonx,
-            subway tile paleo thundercats church-key synth hot chicken.
-          </p>
+          <img src={DevWorkIcon} alt="Work UDraw Illustration" />
         </div>
-        <div>
-          <h4 className={styles.subtitle}>Astro Birth Day</h4>
-          <a
-            href="https://github.com/fiddle-leaf/astro-birth-day"
-            alt="Astro Birth Day on Github"
-          >
-            Source
-          </a>
-          <p>
-            Venmo crucifix williamsburg, literally af tonx mumblecore
-            single-origin coffee ethical church-key raclette. 3 wolf moon offal
-            taiyaki sustainable bruh swag.
-          </p>
-        </div>
-      </article>
+        <article className={styles.cards}>
+          <div>
+            <h4 className={styles.subtitle}>Vacation Planner</h4>
+            <a
+              href="https://github.com/fiddle-leaf/vacations"
+              alt="Vacation Planner on Github"
+            >
+              Source
+            </a>
+            <p>
+              Austin fashion axe bruh blog freegan DIY mustache tote bag tonx,
+              subway tile paleo thundercats church-key synth hot chicken.
+            </p>
+          </div>
+          <div>
+            <h4 className={styles.subtitle}>Astro Birth Day</h4>
+            <a
+              href="https://github.com/fiddle-leaf/astro-birth-day"
+              alt="Astro Birth Day on Github"
+            >
+              Source
+            </a>
+            <p>
+              Venmo crucifix williamsburg, literally af tonx mumblecore
+              single-origin coffee ethical church-key raclette. 3 wolf moon
+              offal taiyaki sustainable bruh swag.
+            </p>
+          </div>
+        </article>
+      </div>
     </article>
   );
 }
